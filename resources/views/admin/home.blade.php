61e10eab040c24a864feeaeebf49ad66f50b28cb
MUSHANIF <hanifmusthafa2005@gmail.com>
2022-09-29 17:03:37 +0700

    membenarkan responsive last final

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -268,7 +268,9 @@
             
             <hr class="sidebar-divider d-none d-md-block">
 
-        
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
 
             
 
@@ -398,6 +400,7 @@
     <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
     <script src="/vendor/jquery/jquery.min.js"></script>
     <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/sb-admin-2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
  
 
